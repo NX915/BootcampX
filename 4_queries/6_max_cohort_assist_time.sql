4cd2dbfb544fda3d0793c7ac4455440ab0631def
Sean Ni <nixiang915@hotmail.com>
2020-09-15 20:03:37 +0000

    complete queries 4

--- 4_queries/6_max_cohort_assist_time.sql
+++ 4_queries/6_max_cohort_assist_time.sql
@@ -1,6 +1,21 @@
-SELECT cohorts.name as name, avg(completed_at - started_at) as average_assistance_time
-FROM assistance_requests
-JOIN students ON student_id = students.id
-JOIN cohorts ON cohort_id = cohorts.id
+-- SELECT cohorts.name, avg(completed_at - started_at) as average_assistance_time
+-- FROM assistance_requests 
+-- JOIN students ON students.id = assistance_requests.student_id
+-- JOIN cohorts ON cohorts.id = cohort_id
+-- GROUP BY cohorts.name
+-- HAVING avg(completed_at - started_at) = (
+--   SELECT max(average_assistance_time) FROM (
+--   SELECT avg(completed_at - started_at) as average_assistance_time
+--   FROM assistance_requests 
+--   JOIN students ON students.id = assistance_requests.student_id
+--   JOIN cohorts ON cohorts.id = cohort_id
+--   GROUP BY cohorts.name) as average
+-- );
+
+SELECT cohorts.name, avg(completed_at - started_at) as average_assistance_time
+FROM assistance_requests 
+JOIN students ON students.id = assistance_requests.student_id
+JOIN cohorts ON cohorts.id = cohort_id
 GROUP BY cohorts.name
-ORDER BY average_assistance_time;
+ORDER BY average_assistance_time DESC
+LIMIT 1;
