27fd599e2b10114e0cd13d1c62055686c6648920
Sean Ni <nixiang915@hotmail.com>
2020-09-16 23:43:17 +0000

    add node js query scripts

--- bootcamp_app/teachers.js
+++ bootcamp_app/teachers.js
@@ -8,16 +8,18 @@ const pool = new Pool({
 });
 
 pool.query(`
-SELECT students.id, students.name as name, cohorts.name as cohort
-FROM students
+SELECT DISTINCT teachers.name as teacher, cohorts.name as cohort
+FROM teachers
+JOIN assistance_requests ON teachers.id = teacher_id
+JOIN students ON student_id = students.id
 JOIN cohorts ON cohort_id = cohorts.id
 WHERE cohorts.name LIKE UPPER('%${process.argv[2]}%')
-ORDER BY students.id
-LIMIT ${process.argv[3]};
+OR cohorts.name = ('JUL02')
+ORDER BY teacher;
 `)
   .then(res => {
     res.rows.forEach(user => {
-      console.log(`${user.name} has an id of ${user.id} and was in the ${user.cohort} cohort`);
+      console.log(`${user.cohort}: ${user.teacher}`);
     });
   })
   .catch(err => console.error('query error', err.stack));
