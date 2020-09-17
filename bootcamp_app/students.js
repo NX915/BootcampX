52cbdfabab4d225113ea827bda5559cea7195643
Sean Ni <nixiang915@hotmail.com>
2020-09-17 00:13:24 +0000

    add sql injection prevention

--- bootcamp_app/students.js
+++ bootcamp_app/students.js
@@ -7,14 +7,21 @@ const pool = new Pool({
   database: 'bootcampx'
 });
 
+const cohortName = process.argv[2];
+const limit = process.argv[3] || 5;
+// Store all potentially malicious values in an array.
+const values = [`%${cohortName}%`, limit];
+
 pool.query(`
 SELECT students.id, students.name as name, cohorts.name as cohort
 FROM students
 JOIN cohorts ON cohort_id = cohorts.id
-WHERE cohorts.name LIKE UPPER('%${process.argv[2]}%')
+WHERE cohorts.name LIKE UPPER($1)
 ORDER BY students.id
-LIMIT ${process.argv[3]};
-`)
+LIMIT $2;
+`,
+values
+)
   .then(res => {
     res.rows.forEach(user => {
       console.log(`${user.name} has an id of ${user.id} and was in the ${user.cohort} cohort`);
